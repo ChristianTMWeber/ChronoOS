0aa128d5d50c0be0f08bef05510e6d998c5f5645
Christian Weber <ChristianTMWeber@gmail.com>
2016-09-23 15:47:31 -0400

    The baseline ChronopixelOS version. Appropriate for the use with the PCB version of the Chronopixel (i.e. the switched traces on the PCB have been acommondated) It transmits the number of activated pixels as comma delimited 4-digit integers (e.g. 0018,0210,0274,0374,0373,0018) followed by a carriage return and a line feed.

---
+++ ChronoOSHeader.h
@@ -0,0 +1,15 @@
+#include <16F1527.h>
+#device adc=16
+
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES WDT_SW                   //No Watch Dog Timer, enabled in Software
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES HS                       //High speed Osc (> 4mhz for PCM/PCH) (>10mhz for PCD)
+//#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOCLKOUT
+#FUSES NOWRT
+
+
+#use delay(clock=20000000)//#use delay(clock=32000000)
+#use rs232(baud=9600,parity=N,xmit=PIN_G1,rcv=PIN_G2,bits=8)
+
