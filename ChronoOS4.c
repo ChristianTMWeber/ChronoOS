2b94b18355d46910fa7e77966c721511dbf13188
Christian Weber <Christian.Weber@yale.edu>
2016-09-28 11:09:39 -0500

    Replaces the void waveform_mrst4() definition with the one from Chronopixel10

--- ChronoOS4.c
+++ ChronoOS4.c
@@ -1044,34 +1044,33 @@ void waveform_mrst4()            // Memory Reset Test section
     tnin_high();
     pdrst_high();
     set_high();
-    delay_us(25);
+    delay_us(6);//25,12
     pdrst_low();
-    delay_us(25);
+    delay_us(6);//25,12
     tnin_low();
-    delay_us(13);
+    delay_us(3);//13,6
     tin_high();
     set_low();
-    delay_us(25);
+    delay_us(6);//25,12
     tin_low();                 //t11
-    delay_us(5);
+    delay_us(2);//5,2
     tnin_high();
-    delay_us(37);
+    delay_us(9);//37,18
     tnin_low();
-    delay_us(37);
+    delay_us(9);//37,18
     tin_high();
     cka_high();
-    delay_us(25);
+    delay_us(6);//25,12
     tin_low();
-//   delay_us(25);// inserted on 3/30
     tnin_high();               //t25
-    delay_us(25);
+    delay_us(6);//25,12
     cka_low();
     tnin_low();
-    delay_us(75);
+    delay_us(18);//75,36
     tin_high();                //t38
-    delay_us(25);
+    delay_us(6);//25,12
     tin_low();
-    delay_us(25);
+//   delay_us(12);//25
 }
 
 
