6b83e1464905ed2609e9e4e55b6872dde6e7a385
Christian Weber <Christian.Weber@yale.edu>
2016-09-30 10:36:47 -0500

    Replaced the function sending integers to the RS232 port
    Replaced the four_digit_display() function with one that is capable of
    handling an up to 16 bit integer and that takes not an actual argument.

--- ChronoOS4.c
+++ ChronoOS4.c
@@ -189,7 +189,7 @@ void hit_imlar_zero_low(void);
 void readout(void);
 
 void get_switch(void);
-void four_digit_display(void);
+void fourOrFive_digit_display(long serialOutInteger); // sends the argument integer to the serial port
 void waveform_drdtst(void);
 void waveform_mrst4(void);
 void waveform_idle4(void);
@@ -425,32 +425,24 @@ void main()
         }
         
 // Display the first group of columns
-        total_chrono_count = column_count1;
-        four_digit_display();
+        fourOrFive_digit_display(column_count1);
 // Display the second group of columns
         putc(0x2C); //comma
-        total_chrono_count = column_count2;
-        four_digit_display();
+        fourOrFive_digit_display(column_count2);
 // Display the third group of columns
         putc(0x2C); //comma
-        total_chrono_count = column_count3;
-        four_digit_display();
+        fourOrFive_digit_display(column_count3);
 // Display the fourth group of columns
         putc(0x2C); //comma
-        total_chrono_count = column_count4;
-        four_digit_display();
+        fourOrFive_digit_display(column_count4);
 // Display the fifth group of columns
         putc(0x2C); //comma
-        total_chrono_count = column_count5;
-        four_digit_display();
+        fourOrFive_digit_display(column_count5);
 // Display the sixth group of columns
         putc(0x2C); //comma
-        total_chrono_count = column_count6;
-        four_digit_display();
+        fourOrFive_digit_display(column_count6);
         putc(0x0d);		// CR (carriage return) and
         putc(0x0a);   	// LF (linefeed) between pixels
-
-
     }     // End of while(TRUE)
 
 }  // End of main
@@ -725,33 +717,116 @@ void read_chrono()                        // Increments the row and column count
     
 }
 
-void four_digit_display()
+// sends the argument integer to the serial port
+// capable of sending a 16 bit unsigned integer
+// transmits four of five digits, depending on whether the 10^4 digit is 0 or not
+void fourOrFive_digit_display(long serialOutInteger)
 {
 //
+//Tenthousands
+    if (serialOutInteger >= 10000)
+    {
+        /*if (serialOutInteger >= 90000)
+        {
+            display_count = 0x39;
+            serialOutInteger = (serialOutInteger - 90000);
+        }
+        if (serialOutInteger >= 80000)
+        {
+            display_count = 0x38;
+            serialOutInteger = (serialOutInteger - 80000);
+        }
+        if (serialOutInteger >= 70000)
+        {
+            display_count = 0x37;
+            serialOutInteger = (serialOutInteger - 70000);
+        } */
+        if (serialOutInteger >= 60000)
+        {
+            display_count = 0x36;
+            serialOutInteger = (serialOutInteger - 60000);
+        }
+        if (serialOutInteger >= 50000)
+        {
+            display_count = 0x35;
+            serialOutInteger = (serialOutInteger - 50000);
+        }
+        if (serialOutInteger >= 40000)
+        {
+            display_count = 0x34;
+            serialOutInteger = (serialOutInteger - 40000);
+        }
+        if (serialOutInteger >= 30000)
+        {
+            display_count = 0x33;
+            serialOutInteger = (serialOutInteger - 30000);
+        }
+        if (serialOutInteger >= 20000)
+        {
+            display_count = 0x32;
+            serialOutInteger = (serialOutInteger - 20000);
+        }
+        if (serialOutInteger >= 10000)
+        {
+            display_count = 0x31;
+            serialOutInteger = (serialOutInteger - 10000);
+        }
+        if (serialOutInteger < 10000)
+        {
+            putc(display_count);
+        }
+    }
+
+
 // Thousands
 //
     display_count = 0x30;
-    if (total_chrono_count >= 4000)
+    if (serialOutInteger >= 9000)
+    {
+        display_count = 0x39;
+        serialOutInteger = (serialOutInteger - 9000);
+    }
+    if (serialOutInteger >= 8000)
+    {
+        display_count = 0x38;
+        serialOutInteger = (serialOutInteger - 8000);
+    }
+    if (serialOutInteger >= 7000)
+    {
+        display_count = 0x37;
+        serialOutInteger = (serialOutInteger - 7000);
+    }
+    if (serialOutInteger >= 6000)
+    {
+        display_count = 0x36;
+        serialOutInteger = (serialOutInteger - 6000);
+    }
+    if (serialOutInteger >= 5000)
+    {
+        display_count = 0x35;
+        serialOutInteger = (serialOutInteger - 5000);
+    }
+    if (serialOutInteger >= 4000)
     {
         display_count = 0x34;
-        total_chrono_count = (total_chrono_count - 4000);
+        serialOutInteger = (serialOutInteger - 4000);
     }
-    if (total_chrono_count >= 3000)
+    if (serialOutInteger >= 3000)
     {
         display_count = 0x33;
-        total_chrono_count = (total_chrono_count - 3000);
+        serialOutInteger = (serialOutInteger - 3000);
     }
-    if (total_chrono_count >= 2000)
+    if (serialOutInteger >= 2000)
     {
         display_count = 0x32;
-        total_chrono_count = (total_chrono_count - 2000);
+        serialOutInteger = (serialOutInteger - 2000);
     }
-    if (total_chrono_count >= 1000)
+    if (serialOutInteger >= 1000)
     {
         display_count = 0x31;
-        total_chrono_count = (total_chrono_count - 1000);
+        serialOutInteger = (serialOutInteger - 1000);
     }
-    if (total_chrono_count < 1000)
+    if (serialOutInteger < 1000)
     {
         putc(display_count);
     }
@@ -760,105 +835,105 @@ void four_digit_display()
 // Hundreds
 //
     display_count = 0x30;
-    if (total_chrono_count >= 900)
+    if (serialOutInteger >= 900)
     {
         display_count = 0x39;
-        total_chrono_count = (total_chrono_count - 900);
+        serialOutInteger = (serialOutInteger - 900);
     }
-    if (total_chrono_count >= 800)
+    if (serialOutInteger >= 800)
     {
         display_count = 0x38;
-        total_chrono_count = (total_chrono_count - 800);
+        serialOutInteger = (serialOutInteger - 800);
     }
-    if (total_chrono_count >= 700)
+    if (serialOutInteger >= 700)
     {
         display_count = 0x37;
-        total_chrono_count = (total_chrono_count - 700);
+        serialOutInteger = (serialOutInteger - 700);
     }
-    if (total_chrono_count >= 600)
+    if (serialOutInteger >= 600)
     {
         display_count = 0x36;
-        total_chrono_count = (total_chrono_count - 600);
+        serialOutInteger = (serialOutInteger - 600);
     }
-    if (total_chrono_count >= 500)
+    if (serialOutInteger >= 500)
     {
         display_count = 0x35;
-        total_chrono_count = (total_chrono_count - 500);
+        serialOutInteger = (serialOutInteger - 500);
     }
-    if (total_chrono_count >= 400)
+    if (serialOutInteger >= 400)
     {
         display_count = 0x34;
-        total_chrono_count = (total_chrono_count - 400);
+        serialOutInteger = (serialOutInteger - 400);
     }
-    if (total_chrono_count >= 300)
+    if (serialOutInteger >= 300)
     {
         display_count = 0x33;
-        total_chrono_count = (total_chrono_count - 300);
+        serialOutInteger = (serialOutInteger - 300);
     }
-    if (total_chrono_count >= 200)
+    if (serialOutInteger >= 200)
     {
         display_count = 0x32;
-        total_chrono_count = (total_chrono_count - 200);
+        serialOutInteger = (serialOutInteger - 200);
     }
-    if (total_chrono_count >= 100)
+    if (serialOutInteger >= 100)
     {
         display_count = 0x31;
-        total_chrono_count = (total_chrono_count - 100);
+        serialOutInteger = (serialOutInteger - 100);
     }
     putc(display_count);
 //
 // Tens
 //
     display_count = 0x30;
-    if (total_chrono_count >= 90)
+    if (serialOutInteger >= 90)
     {
         display_count = 0x39;
-        total_chrono_count = (total_chrono_count - 90);
+        serialOutInteger = (serialOutInteger - 90);
     }
-    if (total_chrono_count >= 80)
+    if (serialOutInteger >= 80)
     {
         display_count = 0x38;
-        total_chrono_count = (total_chrono_count - 80);
+        serialOutInteger = (serialOutInteger - 80);
     }
-    if (total_chrono_count >= 70)
+    if (serialOutInteger >= 70)
     {
         display_count = 0x37;
-        total_chrono_count = (total_chrono_count - 70);
+        serialOutInteger = (serialOutInteger - 70);
     }
-    if (total_chrono_count >= 60)
+    if (serialOutInteger >= 60)
     {
         display_count = 0x36;
-        total_chrono_count = (total_chrono_count - 60);
+        serialOutInteger = (serialOutInteger - 60);
     }
-    if (total_chrono_count >= 50)
+    if (serialOutInteger >= 50)
     {
         display_count = 0x35;
-        total_chrono_count = (total_chrono_count - 50);
+        serialOutInteger = (serialOutInteger - 50);
     }
-    if (total_chrono_count >= 40)
+    if (serialOutInteger >= 40)
     {
         display_count = 0x34;
-        total_chrono_count = (total_chrono_count - 40);
+        serialOutInteger = (serialOutInteger - 40);
     }
-    if (total_chrono_count >= 30)
+    if (serialOutInteger >= 30)
     {
         display_count = 0x33;
-        total_chrono_count = (total_chrono_count - 30);
+        serialOutInteger = (serialOutInteger - 30);
     }
-    if (total_chrono_count >= 20)
+    if (serialOutInteger >= 20)
     {
         display_count = 0x32;
-        total_chrono_count = (total_chrono_count - 20);
+        serialOutInteger = (serialOutInteger - 20);
     }
-    if (total_chrono_count >= 10)
+    if (serialOutInteger >= 10)
     {
         display_count = 0x31;
-        total_chrono_count = (total_chrono_count - 10);
+        serialOutInteger = (serialOutInteger - 10);
     }
     putc(display_count);
     
-    total_chrono_count = (total_chrono_count + 0x30);
-    putc(total_chrono_count);
+    serialOutInteger = (serialOutInteger + 0x30);
+    putc(serialOutInteger);
     
     
 }
